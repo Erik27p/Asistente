b0c2429bcf9925a10a7df2a1597a8963ddd4f1c1
Erik27p <158311406+Erik27p@users.noreply.github.com>
2024-03-18 12:06:45 -0500

    Add files via upload

---
+++ iahistoria.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from ia import answer_question
+
+app = Flask(__name__, template_folder='C:/Users/JANUS/Documents/iahis', static_folder='C:/Users/JANUS/Documents/iahis/static')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/answer', methods=['POST'])
+def get_answer():
+    question = request.form['question']
+    answer = answer_question(question)
+    return render_template('ans.html', question=question, answer=answer)
+
+if __name__ == '__main__':
+    app.run(debug=True)
